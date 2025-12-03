78bbecc06560dc77ef7d0a39c04cab15a6cd7e65
Paul Cormier <lastloginname@gmail.com>
2025-12-03 08:53:10 -0500

    Day 3 - Part 1

---
+++ src/main/java/aoc/_2025/Day03.java
@@ -0,0 +1,142 @@
+package aoc._2025;
+
+import java.util.List;
+
+import org.slf4j.LoggerFactory;
+
+import aoc.FileUtils;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+
+/**
+ * https://adventofcode.com/2025/day/03
+ * 
+ * @author Paul Cormier
+ *
+ */
+public class Day03 {
+
+    private static final Logger log = ((LoggerContext) LoggerFactory.getILoggerFactory()).getLogger(Day03.class);
+
+    private static final String INPUT_TXT = "input/Day03.txt";
+
+    private static final String TEST_INPUT_TXT = "testInput/Day03.txt";
+
+
+
+    public static void main(String[] args) {
+
+        var resultMessage = "The total output joltage is {}";
+
+        log.info("Part 1:");
+        log.setLevel(Level.DEBUG);
+
+        // Read the test file
+        List<String> testLines = FileUtils.readFile(TEST_INPUT_TXT);
+
+        var expectedTestResult = 357;
+        var testResult = part1(testLines);
+
+        log.info("Should be {}", expectedTestResult);
+        log.info(resultMessage, testResult);
+
+        if (testResult != expectedTestResult)
+            log.error("The test result doesn't match the expected value.");
+
+        log.setLevel(Level.INFO);
+
+        // Read the real file
+        List<String> lines = FileUtils.readFile(INPUT_TXT);
+
+        log.info(resultMessage, part1(lines));
+
+        // PART 2
+        resultMessage = "{}";
+
+        log.info("Part 2:");
+        log.setLevel(Level.DEBUG);
+
+        expectedTestResult = 1_234_567_890;
+        testResult = part2(testLines);
+
+        log.info("Should be {}", expectedTestResult);
+        log.info(resultMessage, testResult);
+
+        if (testResult != expectedTestResult)
+            log.error("The test result doesn't match the expected value.");
+
+        log.setLevel(Level.INFO);
+
+        log.info(resultMessage, part2(lines));
+    }
+
+
+
+    /**
+     * Find the largest two-digit number that can be read from left to right in
+     * each input line, and return the sum.
+     * 
+     * @param lines The lines read from the input.
+     * @return The value calculated for part 1.
+     */
+    private static long part1(final List<String> lines) {
+
+        return lines.stream()
+                    .mapToInt(Day03::findLargestJoltage)
+                    .sum();
+
+    }
+
+
+
+    /**
+     * Scan a line of digits and find the largest two-digit number that can be
+     * read from left to right.
+     * 
+     * @param line The line of digits to scan.
+     * @return The value of the largest two-digit number that can be read from
+     *         left to right.
+     */
+    private static int findLargestJoltage(String line) {
+
+        char firstDigit = '?';
+
+        for (int i = 9; i > 0; i--) {
+            int index = line.indexOf(i + '0');
+            if (index >= 0 && index < line.length() - 1) {
+                firstDigit = line.charAt(index);
+                break;
+            }
+        }
+
+        String subString = line.substring(line.indexOf(firstDigit) + 1);
+
+        char secondDigit = '?';
+        for (int i = 9; i > 0; i--) {
+            int index = subString.indexOf(i + '0');
+            if (index >= 0) {
+                secondDigit = subString.charAt(index);
+                break;
+            }
+        }
+
+        var joltage = Integer.valueOf("" + firstDigit + secondDigit);
+        log.debug("In {} the largest joltage possible is {}.", line, joltage);
+
+        return joltage;
+    }
+
+
+
+    /**
+     * 
+     * @param lines The lines read from the input.
+     * @return The value calculated for part 2.
+     */
+    private static long part2(final List<String> lines) {
+
+        return -1;
+    }
+
+}
