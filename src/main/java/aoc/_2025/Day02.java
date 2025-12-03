0d8bdff3c0eada493938d00ae2b2c8308ebe57a7
Paul Cormier <lastloginname@gmail.com>
2025-12-02 20:12:23 -0500

    Day 2 - Part 2

--- src/main/java/aoc/_2025/Day02.java
+++ src/main/java/aoc/_2025/Day02.java
@@ -43,7 +43,7 @@ public class Day02 {
                                                          .getFirst()
                                                          .split(","));
 
-        var expectedTestResult = 1_227_775_554;
+        var expectedTestResult = 1_227_775_554L;
         var testResult = part1(testRanges);
 
         log.info("Should be {}", expectedTestResult);
@@ -52,7 +52,7 @@ public class Day02 {
         if (testResult != expectedTestResult)
             log.error("The test result doesn't match the expected value.");
 
-        log.setLevel(Level.TRACE);
+        log.setLevel(Level.INFO);
 
         // Read the real file
         List<String> ranges = Arrays.asList(FileUtils.readFile(INPUT_TXT)
@@ -65,9 +65,9 @@ public class Day02 {
         resultMessage = "{}";
 
         log.info("Part 2:");
-        log.setLevel(Level.DEBUG);
+        log.setLevel(Level.TRACE);
 
-        expectedTestResult = 1_234_567_890;
+        expectedTestResult = 4_174_379_265L;
         testResult = part2(testRanges);
 
         log.info("Should be {}", expectedTestResult);
@@ -88,6 +88,7 @@ public class Day02 {
      * digits repeated twice.
      * 
      * @param ranges The ranges read from the input.
+     * 
      * @return The sum of all "invalid" IDs.
      */
     private static long part1(final List<String> ranges) {
@@ -114,10 +115,27 @@ public class Day02 {
      * doubled sequence of digits.
      * 
      * @param range The range in which to search.
+     * 
      * @return A list of IDs which are within the given range, and are made up
      *         of a sequence of digits which is repeated once.
      */
     private static List<Long> findDoubledIDs(Range<Long> range) {
+        return findRepeatedIDs(range, 2);
+    }
+
+
+
+    /**
+     * Given a particular range, find IDs in the range which are exactly a
+     * sequence of digits repeated a certain number of times.
+     * 
+     * @param range The range in which to search.
+     * @param repeats The number of times the sequence is to be repeated.
+     * 
+     * @return A list of IDs which are within the given range, and are made up
+     *         of a sequence of digits which is repeated once.
+     */
+    private static List<Long> findRepeatedIDs(Range<Long> range, int repeats) {
 
         log.debug("Range {}", range);
 
@@ -126,19 +144,20 @@ public class Day02 {
         // Pad the min to the same number of digits as the max
         var min = StringUtils.leftPad(range.getMinimum().toString(), max.length(), '0');
 
-                
         Range<Integer> halfRange;
         try {
-            halfRange = Range.of(Integer.valueOf(min.substring(0,(int) Math.round(min.length() / 2.))),
-                                 Integer.valueOf(max.substring(0,(int) Math.round(max.length() / 2.))));
+            // Gradually trim off the right digits (least significant values) of each number in the range.
+            var digitsToTrim = max.length() / repeats * (repeats - 1);
+            halfRange = Range.of(Integer.valueOf(min.substring(0, max.length() - digitsToTrim)),
+                                 Integer.valueOf(max.substring(0, min.length() - digitsToTrim)));
         } catch (NumberFormatException nfe) {
             log.error("Could not create a range from {} ({}-{})", range, min, max, nfe);
             return Collections.emptyList();
         }
-        log.trace("Checking {}", halfRange);
+        log.trace("Checking {}, {} times", halfRange, repeats);
 
         return IntStream.rangeClosed(halfRange.getMinimum(), halfRange.getMaximum())
-                        .mapToLong(i -> Long.valueOf("%d%1$d".formatted(i)))
+                        .mapToLong(i -> Long.valueOf(StringUtils.repeat("%1$d", repeats).formatted(i)))
                         .filter(range::contains)
                         .boxed()
                         .toList();
@@ -147,13 +166,34 @@ public class Day02 {
 
 
     /**
+     * Given a particular range, find IDs in the range which are exactly a
+     * repeated sequence of digits.
      * 
-     * @param lines The lines read from the input.
+     * @param ranges The lines read from the input.
+     * 
      * @return The value calculated for part 2.
      */
-    private static long part2(final List<String> lines) {
+    private static long part2(final List<String> ranges) {
 
-        return -1;
+        return ranges.stream()
+                     .map(r -> Range.of(Long.valueOf(r.split("-")[0]),
+                                        Long.valueOf(r.split("-")[1])))
+                     // Try all the multiples from 2 to the length of the longest string
+                     .map(r -> IntStream.rangeClosed(2, r.getMaximum().toString().length())
+                                        // Skip anything that isn't a multiple of one of the string's lengths
+                                        .filter(i -> r.getMaximum().toString().length() % i == 0 ||
+                                                     r.getMinimum().toString().length() % i == 0)
+                                        .mapToObj(i -> findRepeatedIDs(r, i))
+                                        .flatMap(Collection::stream)
+                                        .distinct()
+                                        .toList())
+                     .peek(l -> {
+                         if (!l.isEmpty())
+                             log.debug("Invalid IDs {}", l);
+                     })
+                     .flatMap(Collection::stream)
+                     .mapToLong(Long::longValue)
+                     .sum();
     }
 
 }
