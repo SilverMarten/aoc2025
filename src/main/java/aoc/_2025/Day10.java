264d219f9cb7c166dc85202f33c136b65d0dade4
Paul Cormier <lastloginname@gmail.com>
2025-12-10 20:27:44 -0500

    Day 10 - Part 1 parse and print input

--- src/main/java/aoc/_2025/Day10.java
+++ src/main/java/aoc/_2025/Day10.java
@@ -1,6 +1,11 @@
 package aoc._2025;
 
+import static java.util.stream.Collectors.joining;
+
+import java.util.BitSet;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import org.slf4j.LoggerFactory;
 
@@ -87,6 +92,11 @@ public class Day10 {
                                       .map(Machine::fromLine)
                                       .toList();
 
+        log.atDebug()
+           .setMessage("\n{}")
+           .addArgument(() -> machines.stream().map(Machine::toString).collect(joining("\n")))
+           .log();
+
         // Run the machine, and press buttons until the desired state is reached
         return machines.stream()
                        .mapToInt(Day10::runToStart)
@@ -125,6 +135,33 @@ public class Day10 {
 
     private static class Machine {
 
+        /** The condition the light needs to be in to start. */
+        private BitSet startLight;
+
+        /** The buttons for this machine. */
+        private List<BitSet> buttons;
+
+        /** The joltages for this machine. */
+        private List<Integer> joltages;
+
+
+
+        /**
+         * Construct a new {@link Machine} with the given values.
+         * 
+         * @param startLight The value of the lights which indicates the machine
+         *            should start.
+         * @param buttons The values of the buttons for the machine.
+         * @param joltages The values of the joltages for the machine.
+         */
+        public Machine(BitSet startLight, List<BitSet> buttons, List<Integer> joltages) {
+            this.startLight = startLight;
+            this.buttons = buttons;
+            this.joltages = joltages;
+        }
+
+
+
         /**
          * Given a line containing a machine configuration, a single indicator
          * light diagram in [square brackets], one or more button wiring
@@ -137,15 +174,58 @@ public class Day10 {
          *         string.
          */
         public static Machine fromLine(String line) {
-            return new Machine();
+            // Light diagram
+            BitSet light = new BitSet();
+            IntStream.range(1, line.lastIndexOf(']'))
+                     .forEach(i -> light.set(i, line.charAt(i) == '#'));
+
+            // Button wiring
+            List<BitSet> buttons = Stream.of(line.substring(line.indexOf('(') + 1, line.lastIndexOf(')')).split("\\) \\("))
+                                         .map(b -> {
+                                             var bits = new BitSet();
+                                             Stream.of(b.split(","))
+                                                   .map(Integer::valueOf)
+                                                   .forEach(bits::set);
+                                             return bits;
+                                         })
+                                         .toList();
+
+            // Joltages
+            List<Integer> joltages = Stream.of(line.substring(line.indexOf('{') + 1, line.length() - 1).split(","))
+                                           .map(Integer::valueOf)
+                                           .toList();
+
+            return new Machine(light, buttons, joltages);
         }
 
 
 
         @Override
         public String toString() {
-            // TODO Auto-generated method stub
-            return super.toString();
+            StringBuilder string = new StringBuilder();
+
+            // Light diagram
+            char[] lightString = ".".repeat(this.joltages.size()).toCharArray();
+            this.startLight.stream()
+                           .limit(this.joltages.size())
+                           .forEach(i -> lightString[i-1] = '#');
+            string.append("[").append(lightString).append("] ");
+
+            // Button wiring
+            String buttonString = this.buttons.stream()
+                                              .map(b -> b.stream()
+                                                         .mapToObj(Integer::toString)
+                                                         .collect(joining(",")))
+                                              .collect(joining(") ("));
+            string.append("(").append(buttonString).append(") ");
+
+            // Joltages
+            String joltageString = this.joltages.stream()
+                                                .map(Object::toString)
+                                                .collect(joining(","));
+            string.append("{").append(joltageString).append("}");
+
+            return string.toString();
         }
     }
 
